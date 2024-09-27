bdbe70291e9bf714907b2e84a54df90e676ba102
FrostAce52 <ezioallen@gmail.com>
2024-09-27 13:58:19 +0800

    todo-remove-toggle

--- pages/cs-0927/todo.js
+++ pages/cs-0927/todo.js
@@ -9,12 +9,34 @@ export default function Todo(props) {
   // 專門為了讓文字輸入框ui綁定的狀態
   const [inputText, setInputText] = useState('abc')
 
+  // 新增todo
   const handleAdd = (text) => {
-    // 先寫出要新增的物件值
-    const newTodo = { id: Number(new Date()), text }
-    setTodos([newTodo, ...todos])
-    // 清空文字輸入框
-    setInputText('')
+    // 如果文字不是空白時再作新增
+    if (text) {
+      // 先寫出要新增的物件值
+      const newTodo = { id: Number(new Date()), text }
+      setTodos([newTodo, ...todos])
+      // 清空文字輸入框
+      setInputText('')
+    }
+  }
+  // 刪除todo
+  const handleRemove = (id) => {
+    const nextTodos = todos.filter((v) => v.id !== id)
+    setTodos(nextTodos)
+  }
+
+  // 切換todo項目的completed屬性
+  const handleToggleCompleted = (id) => {
+    const nextTodos = todos.map((v, i) => {
+      // 這裡判斷id值是否等於傳入id
+      if (v.id === id) {
+        return { ...v, completed: !v.completed }
+      } else {
+        return v
+      }
+    })
+    setTodos(nextTodos)
   }
   return (
     <>
@@ -27,6 +49,13 @@ export default function Todo(props) {
         onChange={(e) => {
           setInputText(e.target.value)
         }}
+        placeholder="輸入文字"
+        onKeyDown={(e) => {
+          // 如果按下是Enter鍵時
+          if (e.key === 'Enter') {
+            handleAdd(inputText)
+          }
+        }}
       />
       <button
         onClick={() => {
@@ -35,11 +64,55 @@ export default function Todo(props) {
       >
         加入
       </button>
+      <hr />
       <ul>
         {todos.map((todo) => {
-          return <li key={todo.id}>{todo.text}</li>
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => {
+                  handleToggleCompleted(todo.id)
+                }}
+              />
+              <span
+                className={todo.completed ? 'completed' : 'active'}
+                // style={{
+                //   color: todo.completed ? 'gray' : 'green',
+                //   textDecoration: todo.completed ? 'line-through' : 'none',
+                //   fontWeight: todo.completed ? 'normal' : 'bold',
+                // }}
+              >
+                {todo.text}
+              </span>
+
+              <button
+                onClick={() => {
+                  handleRemove(todo.id)
+                }}
+              >
+                刪除
+              </button>
+            </li>
+          )
         })}
       </ul>
+      <style jsx>
+        {`
+          .completed {
+            color: gray;
+            text-decoration: line-through;
+            font-weight: normal;
+          }
+
+          .active {
+            color: green;
+            text-decoration: none;
+            font-weight: bold;
+          }
+        `}
+      </style>
     </>
   )
 }
