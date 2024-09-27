60911fd9dc5430442be8d4a60ac681167b773fb6
FrostAce52 <ezioallen@gmail.com>
2024-09-27 10:21:50 +0800

    immer-sample

--- pages/cs-0927/immer-sample.js
+++ pages/cs-0927/immer-sample.js
@@ -1,9 +1,53 @@
 import React, { useState, useEffect } from 'react'
+import { produce } from 'immer'
 
 export default function ImmerSample(props) {
+  const [user, setUser] = useState({
+    id: 1,
+    name: 'Nike',
+    address: {
+      country: {
+        city: 'Taipei City',
+      },
+    },
+  })
+
   return (
     <>
-      <div>immer使用範例</div>
+      <h1>Immer應用範例</h1>
+      <hr />
+      <p>姓名: {user.name}</p>
+      <p>城市: {user.address.country.city}</p>
+      <button
+        onClick={() => {
+          const nextUser = {
+            ...user,
+            name: 'Iris',
+          }
+
+          setUser(nextUser)
+        }}
+      >
+        修改姓名為Iris
+      </button>
+      <button
+        onClick={() => {
+          const nextUser = {
+            ...user,
+            address: {
+              ...user.address,
+              country: {
+                ...user.address.country,
+                city: 'New Taipei City',
+              },
+            },
+          }
+
+          setUser(nextUser)
+        }}
+      >
+        修改城市為New Taipei City
+      </button>
     </>
   )
 }
