1b50896a78b7a9df8198ee5359c974894699483d
FrostAce52 <ezioallen@gmail.com>
2024-09-27 14:49:32 +0800

    todo-filtertype

--- pages/cs-0927/todo.js
+++ pages/cs-0927/todo.js
@@ -1,28 +1,45 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Todo(props) {
+  // 宣告待辨事項使用的物件陣列狀態
+  // 擴充completed屬性(布林值)，代表是否已完成(已完成=true)
   const [todos, setTodos] = useState([
-    { id: 1, text: '買牛奶' },
-    { id: 2, text: '學react' },
+    { id: 1, text: '買牛奶', completed: true },
+    { id: 2, text: '學react', completed: false },
   ])
 
-  // 專門為了讓文字輸入框ui綁定的狀態
-  const [inputText, setInputText] = useState('abc')
+  // 用於呈現的todos(後續CRUD處理繁雜)
+  // const [todosDisplay, setTodosDisplay] = useState([
+  //   { id: 1, text: '買牛奶', completed: true },
+  //   { id: 2, text: '學react', completed: false },
+  // ])
+
+  // 宣告專門讓文字輸入框ui綁定的狀態
+  const [inputText, setInputText] = useState('')
+
+  // 過濾類型('所有', '進行中', '已完成')
+  const [filterType, setFilterType] = useState('所有')
+
+  // 呈現過濾的選項
+  const filterOptions = ['所有', '進行中', '已完成']
 
   // 新增todo
   const handleAdd = (text) => {
     // 如果文字不是空白時再作新增
     if (text) {
-      // 先寫出要新增的物件值
-      const newTodo = { id: Number(new Date()), text }
+      // 寫出要新增的物件值(這裡記得在新增時，也要加上預設的`completed: false`屬性)
+      const newTodo = { id: Date.now(), text, completed: false }
+      // 設定到狀態
       setTodos([newTodo, ...todos])
       // 清空文字輸入框
       setInputText('')
     }
   }
+
   // 刪除todo
   const handleRemove = (id) => {
     const nextTodos = todos.filter((v) => v.id !== id)
+    // 設定到狀態
     setTodos(nextTodos)
   }
 
@@ -31,21 +48,38 @@ export default function Todo(props) {
     const nextTodos = todos.map((v, i) => {
       // 這裡判斷id值是否等於傳入id
       if (v.id === id) {
+        // 這裡切換todo項目的completed屬性
         return { ...v, completed: !v.completed }
       } else {
         return v
       }
     })
+
+    // 設定到狀態
     setTodos(nextTodos)
   }
+
+  // 處理不同類型的呈現
+  const getTodosByFilterType = () => {
+    if (filterType === '已完成') {
+      return todos.filter((v) => v.completed)
+    }
+    if (filterType === '進行中') {
+      return todos.filter((v) => !v.completed)
+    }
+    return todos
+  }
+
   return (
     <>
-      <h1>待辦事項</h1>
+      <h1>待辨事項</h1>
       <hr />
       {/* 完全(雙向)綁定到狀態的表單元件，稱為controlled(可控的/受控的) */}
       <input
         type="text"
+        // value屬性要對應到某個狀態
         value={inputText}
+        // onChange事件要對應到value對應狀態的更動情況
         onChange={(e) => {
           setInputText(e.target.value)
         }}
@@ -59,6 +93,7 @@ export default function Todo(props) {
       />
       <button
         onClick={() => {
+          // 加入todo
           handleAdd(inputText)
         }}
       >
@@ -66,18 +101,22 @@ export default function Todo(props) {
       </button>
       <hr />
       <ul>
-        {todos.map((todo) => {
+        {getTodosByFilterType().map((todo) => {
           return (
             <li key={todo.id}>
               <input
                 type="checkbox"
+                // 核取方塊的呈現是對應checked屬性(布林值)
                 checked={todo.completed}
+                // onChange事件要對應到value對應狀態的更動情況
                 onChange={() => {
                   handleToggleCompleted(todo.id)
                 }}
               />
               <span
+                // 利用狀態改變來切換不同的樣式
                 className={todo.completed ? 'completed' : 'active'}
+                // 也可以使用style屬性來切換樣式，適合只有一點點樣式使用
                 // style={{
                 //   color: todo.completed ? 'gray' : 'green',
                 //   textDecoration: todo.completed ? 'line-through' : 'none',
@@ -86,7 +125,6 @@ export default function Todo(props) {
               >
                 {todo.text}
               </span>
-
               <button
                 onClick={() => {
                   handleRemove(todo.id)
@@ -98,6 +136,21 @@ export default function Todo(props) {
           )
         })}
       </ul>
+      <div>
+        {filterOptions.map((v, i) => {
+          return (
+            <button
+              key={i}
+              onClick={() => {
+                setFilterType(v)
+              }}
+            >
+              {v}
+            </button>
+          )
+        })}
+      </div>
+      {/* 這裡示範使用styled-jsx來套用本頁的樣式 */}
       <style jsx>
         {`
           .completed {
