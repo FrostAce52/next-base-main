b0c07436888f3c056013c7da456285517a4bf512
FrostAce52 <ezioallen@gmail.com>
2024-09-30 10:38:48 +0800

    star-hover

--- pages/cs-0930/star.js
+++ pages/cs-0930/star.js
@@ -2,9 +2,14 @@ import { useState } from 'react'
 // 導入.module.css檔案
 import styles from '@/styles/star.module.css'
 
+// 星星評分元件 #60 更進階參考教學、元件:
+// https://github.com/orgs/mfee-react/discussions/60
 export default function Star() {
   // 點按時的評分，一開始是0分代表沒有評分
   const [rating, setRating] = useState(0)
+
+  // 滑鼠游標懸停(hover)點按時的評分，一開始是0分代表沒有評分
+  const [hoverRating, setHoverRating] = useState(0)
 
   return (
     <>
@@ -27,11 +32,23 @@ export default function Star() {
                   // 點按後設定分數
                   setRating(score)
                 }}
+                onMouseEnter={() => {
+                  // 滑鼠游標移入時設定分數(設定hover效果)
+                  setHoverRating(score)
+                }}
+                onMouseLeave={() => {
+                  // 滑鼠游標移出時設定分數為0(取消hover效果)
+                  setHoverRating(0)
+                }}
                 className={styles.starBtn}
               >
                 <span
-                  // 判斷星星是否要點亮。如果這個星星的分數(score)小於等於目前的評分(rating)，則套用亮起樣式
-                  className={score <= rating ? styles.on : styles.off}
+                  // 如果這個星星的分數(score)小於等於評分(rating)，或小於等於滑鼠游標懸停(hover)評分，則套用亮起樣式
+                  className={
+                    score <= rating || score <= hoverRating
+                      ? styles.on
+                      : styles.off
+                  }
                 >
                   &#9733;
                 </span>
