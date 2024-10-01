1619c62132d2b70fd2536c446fcdf6cc9081f3b2
FrostAce52 <ezioallen@gmail.com>
2024-10-01 09:39:11 +0800

    reassemble-book-list item and list

---
+++ components/book-list-app/list.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import Image from 'next/image'
+import Item from './item'
+// 範例資料
+import data from '@/data/books.json'
+
+export default function BookList() {
+  // 擴充一個可以代表是否有加入收藏的(我的最愛)布林值屬性，預設是false
+  const initState = data.map((v) => {
+    return { ...v, fav: false }
+  })
+
+  // 宣告狀態
+  const [books, setBooks] = useState(initState)
+
+  // 切換book項目的fav屬性
+  const handleToggleFav = (isbn) => {
+    const nextBooks = books.map((v) => {
+      // 這裡判斷isbn值是否等於傳入isbn
+      if (v.isbn === isbn) {
+        // 這裡切換項目的fav屬性
+        return { ...v, fav: !v.fav }
+      } else {
+        return v
+      }
+    })
+
+    // 設定到狀態
+    setBooks(nextBooks)
+  }
+
+  return (
+    <>
+      <h1>書籍清單</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ISBN</th>
+            <th>title</th>
+            <th>author</th>
+            <th>加入收藏</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => {
+            return (
+              <Item
+                key={book.isbn}
+                book={book}
+                handleToggleFav={handleToggleFav}
+              />
+            )
+          })}
+        </tbody>
+      </table>
+    </>
+  )
+}
---
+++ pages/cs-1001/book-list.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import List from '@/components/book-list-app/list'
+
+export default function BookList(props) {
+  return (
+    <>
+      <List />
+    </>
+  )
+}
