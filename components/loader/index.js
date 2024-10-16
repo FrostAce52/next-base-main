1e8aca54479f65638dddf240f4210ccc7038a3d1
FrostAce52 <ezioallen@gmail.com>
2024-10-16 10:25:27 +0800

    loading indicator

---
+++ components/loader/index.js
@@ -0,0 +1,13 @@
+import styles from './loader.module.css'
+
+export default function Loader() {
+  return (
+    <>
+      <div className={styles['lds-facebook']}>
+        <div />
+        <div />
+        <div />
+      </div>
+    </>
+  )
+}
---
+++ pages/cs-1016/products/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+
+// 路由: `/products`
+export default function ProductsIndex(props) {
+  return (
+    <>
+      <div>索引檔路由(index.js)</div>
+    </>
+  )
+}
