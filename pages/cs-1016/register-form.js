f2ab6a1cecdf98094cb5d4f54a89044231357870
FrostAce52 <ezioallen@gmail.com>
2024-10-17 09:38:42 +0800

    form-check

--- pages/cs-1016/register-form.js
+++ pages/cs-1016/register-form.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 export default function RegisterForm() {
-  // 狀態為物件，屬性對應到表單的欄位名稱(目的是為了要使用"多欄位共用事件函式")
+  // 狀態為物件，屬性對應到表單的欄位名稱
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -27,15 +27,9 @@ export default function RegisterForm() {
 
   // 多欄位共用事件函式
   const handleFieldChange = (e) => {
-    // 可以用e.target來觀察或是判斷是哪個欄位觸發事件
-    console.log(e.target.type, e.target.name, e.target.value)
-
     // ES6特性: 計算得來的物件屬性名稱(computed property name)
-    // [e.target.name]: e.target.value
-    // ^^^^^^^^^^^^^^^ 這裡可以代入值或表達式，計算出物件的屬性名稱(字串值)
     let nextUser = { ...user, [e.target.name]: e.target.value }
 
-    // agree欄位要特別處理，因為它是要使用checked屬性(布林值)
     if (e.target.name === 'agree') {
       nextUser = { ...user, agree: e.target.checked }
     }
@@ -44,15 +38,69 @@ export default function RegisterForm() {
   }
 
   const handleSubmit = (e) => {
+    // 固定的ajax/fetch的語法，會在表單submit觸發的第一行阻擋表單的預設行為
     e.preventDefault()
 
-    alert('送到伺服器')
+    // 表單檢查--START---
+    // 1. 建立一個全新的錯誤訊息用物件
+    const newErrors = {
+      name: '',
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+      agree: '',
+    }
+
+    // 2.開始作各欄位的表單檢查，如果有錯誤訊息就加到newErrors
+    if (!user.name) {
+      newErrors.name = '姓名為必填'
+    }
+
+    if (!user.email) {
+      newErrors.email = 'Email為必填'
+    }
+
+    if (!user.agree) {
+      newErrors.agree = '請先同意會員註冊條款'
+    }
+
+    if (user.password !== user.confirmPassword) {
+      newErrors.password = '密碼與確認密碼需要相同'
+      newErrors.confirmPassword = '密碼與確認密碼需要相同'
+    }
+
+    if (user.password.length < 6) {
+      newErrors.password = '密碼長度不能小於6'
+    }
+
+    if (!user.password) {
+      newErrors.password = '密碼為必填'
+    }
+
+    if (!user.confirmPassword) {
+      newErrors.confirmPassword = '確認密碼為必填'
+    }
+
+    // 3. 呈現錯誤訊息
+    setErrors(newErrors)
+    // 如果newErrors中的物件值中有非空白字串，代表有錯誤發生
+    const hasErrors = Object.values(newErrors).some((v) => v)
+    // 有錯誤，不送到伺服器，跳出此函式
+    if (hasErrors) {
+      return // 跳出此函式，在下面的程式碼不會再執行
+    }
+    // 表單檢查--END---
+
+    // 送到伺服器，緩衝一些，讓錯誤訊息完全更新後再送到伺服器
+    setTimeout(() => {
+      alert('送到伺服器')
+    }, 0)
   }
 
   return (
     <>
       <h1>註冊表單</h1>
-      {/* 使用form標記時，每個欄位都要給name屬性 */}
       <form onSubmit={handleSubmit}>
         <label>
           姓名:{' '}
@@ -103,7 +151,7 @@ export default function RegisterForm() {
           type="checkbox"
           checked={showPassword}
           onChange={(e) => {
-            setShowPassword(e.target.checked)
+            setShowPassword(!showPassword)
           }}
         />{' '}
         顯示密碼
@@ -123,7 +171,7 @@ export default function RegisterForm() {
           type="checkbox"
           checked={showConfirmPassword}
           onChange={(e) => {
-            setShowConfirmPassword(e.target.checked)
+            setShowConfirmPassword(!showConfirmPassword)
           }}
         />{' '}
         顯示密碼
@@ -134,7 +182,7 @@ export default function RegisterForm() {
           <input
             type="checkbox"
             name="agree"
-            value={user.agree}
+            checked={user.agree}
             onChange={handleFieldChange}
           />{' '}
           我同意網站會員註冊條款
@@ -142,7 +190,7 @@ export default function RegisterForm() {
         <br />
         <span className="error">{errors.agree}</span>
         <br />
-        {/* 建議在form標記中寫button要加type，類型預設是submit */}
+        {/* 在表單(form)中加入button，記得寫type是哪一種，預設不寫是submit */}
         <button type="submit">註冊</button>
         <button type="button" onClick={() => {}}>
           重置
