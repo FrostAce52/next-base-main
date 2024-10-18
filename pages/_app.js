92a2ee64ed8b9559bd13f2ac22781d6e7f2fdcd9
FrostAce52 <ezioallen@gmail.com>
2024-10-18 16:43:56 +0800

    list and npm install react paginate, react icons, bootstrap, and -D sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
+import { useEffect } from 'react'
 import { AuthProvider } from '@/hooks/use-auth'
 import { CartProvider } from '@/hooks/use-cart'
+import '@/styles/globals.scss'
 
 export default function MyApp({ Component, pageProps }) {
   // 使用自訂在頁面層級的版面(layout)
   const getLayout = Component.getLayout || ((page) => page)
 
+  useEffect(() => {
+    // 要document物件出現後才能導入 bootstrap的js函式庫
+    import('bootstrap/dist/js/bootstrap')
+  }, [])
+
   return (
-    <AuthProvider>
-      <CartProvider>{getLayout(<Component {...pageProps} />)}</CartProvider>
-    </AuthProvider>
+    <CartProvider>
+      <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
+    </CartProvider>
   )
 }
